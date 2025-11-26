b167656eb6890902cc756dab70de2ac6cd6d7c2b
Manjunath <manjunath.xworkz@outlook.com>
2025-11-26 23:26:08 +0530

    Delete springbootkickoff directory

--- springbootkickoff/springbootkickoff/src/main/java/com/acuver/springbootkickoff/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.acuver.springbootkickoff.repository;
-
-import com.acuver.springbootkickoff.dto.UserDto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<UserDto,Long> {
-}
--- springbootkickoff/springbootkickoff/src/main/java/com/acuver/springbootkickoff/service/UserService.java
+++
@@ -1,6 +0,0 @@
-package com.acuver.springbootkickoff.service;
-
-import org.springframework.stereotype.Service;
-
-public interface UserService {
-}
